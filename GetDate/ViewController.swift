3097f0bb1752c6e23e66ef8cdd63d451d8117bf9
saul51292 <saulhamadani@me.com>
2015-04-09 15:07:47 -0400

    First Commit
    Signed-off-by: saul51292 <saulhamadani@me.com>

---
+++ GetDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetDate
+//
+//  Created by Saul on 4/9/15.
+//  Copyright (c) 2015 Saul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
